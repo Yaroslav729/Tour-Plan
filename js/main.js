af926c9fc2cface235113f50a8a120a7b0e7c397
Yaroslav729 <company-rosstan@yandex.ru>
2021-05-10 21:21:04 +0300

    create: block-slider, file-HTML, file-JS, file-CSS

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const hotelSwiper = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.hotel-slider__button--next',
+        prevEl: '.hotel-slider__button--prev',
+    },
+    keyboard: {
+        keyboardOptions: 'true',
+    },
+});
